6256acd95d3c63139c3465fcacb6c3c9d9b06af9
ksHl <hashfih12@gmail.com>
2024-03-31 22:54:37 +0700

    tambah class DataMahasiswa(1302220022_PRAKTIKAN)

--- tpmodul7_kelompok_1/Program.cs
+++ tpmodul7_kelompok_1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul7_kelompok_1;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        DataMahasiswa.ReadJSON();
+    }
+}
