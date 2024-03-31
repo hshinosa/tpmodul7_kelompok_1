2124977eeeedfe0b3e004fa9ce0e0116fd0017a8
ksHl <hashfih12@gmail.com>
2024-03-31 22:00:16 +0700

    Commit 3: menambahkan class KuliahMahasiswa(1302220079_PRAKTIKAN)

---
+++ tpmodul7_kelompok_1/KuliahMahasiswa(1302220079_PRAKTIKAN).cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_1
+{
+    class CoursesData
+    {
+        public List<CourseData> courses { get; set; }
+    }
+
+    class CourseData
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    class KuliahMahasiswa
+    {
+        public static void ReadJSON()
+        {
+            Console.WriteLine("\nDaftar Mata Kuliah:");
+
+            // Kode untuk membaca data mata kuliah dari tp7_2_1302220079.json
+            string jsonCourses = File.ReadAllText("D:\\KPL\\tpmodul7_kelompok_1\\tpmodul7_kelompok_1\\tp7_2_1302220079.json");
+            CoursesData courses = JsonSerializer.Deserialize<CoursesData>(jsonCourses);
+
+            foreach (CourseData course in courses.courses)
+            {
+                Console.WriteLine($"Kode: {course.code}, Nama: {course.name}");
+            }
+        }
+    }
+}
