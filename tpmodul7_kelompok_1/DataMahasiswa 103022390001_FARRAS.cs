637d1f6601cb49029fa3ac1eadbb47d0c6f69d7e
Argyaboy20 <smpn21985@gmail.com>
2024-03-31 22:17:15 +0700

    menambahkan class <NAMA_CLASS>

---
+++ tpmodul7_kelompok_1/DataMahasiswa 103022390001_FARRAS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_1
+{
+    internal class DataMahasiswa_103022390001_FARRAS
+    {
+        public NamaData nama { get; set; }
+        public int nim { get; set; }
+        public string fakultas { get; set; }
+
+    }
+
+    class NamaData
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public static class DataMahasiswa
+    {
+        public static void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("D:\\KPL\\tpmodul7_kelompok_1\\tpmodul7_kelompok_1\\tp7_1_1302220079.json");
+            DataMahasiswa_103022390001_FARRAS mahasiswa = JsonSerializer.Deserialize<DataMahasiswa_103022390001_FARRAS>(jsonString);
+
+            Console.WriteLine("Nama " + mahasiswa.nama.depan + " " + mahasiswa.nama.belakang + " dengan nim " + mahasiswa.nim + " dari Fakultas " + mahasiswa.fakultas);
+        }
+    }
+
+}
--- tpmodul7_kelompok_1/Program.cs
+++ tpmodul7_kelompok_1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul7_kelompok_1;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        DataMahasiswa.ReadJSON();
+    }
+}
